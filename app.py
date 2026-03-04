23edc13406685f94329bd4cf1527680657bc1b22
saku-09 <padwalsakshi86@gmail.com>
2026-03-04 11:52:32 +0530

    Added render deployment config

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def webhook():
         return "EVENT_RECEIVED", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
